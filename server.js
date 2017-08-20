03f2f8ea720f6225145b2d09723f970a0497ce4a
subhajitchikky <30743753+subhajitchikky@users.noreply.github.com>
2017-08-20 20:42:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var articleOne ={
             
 };
 function createTemplate(data){
-    var title =data.title;
+    var title = data.title;
     var date = data.date;
     var heading= data.heading;
     var content= data.content;
