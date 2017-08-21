ef540e4a5867af6e8dc32f92696aae28c34b7b02
subhajitchikky <30743753+subhajitchikky@users.noreply.github.com>
2017-08-21 19:58:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 var img= document.getElementById('madi');
 img.onclick= function(){
-    var interval= setInterval(moveLeft, 100);
+    var interval= setInterval(moveRight, 100);
 }
