9c77ff358fd4695d5b2587fe3a2fbc57d21dff95
subhajitchikky <30743753+subhajitchikky@users.noreply.github.com>
2017-08-24 21:22:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,11 @@ app.get('/:articleName',function(req,res){
   
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 
 
 
