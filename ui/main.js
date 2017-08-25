fd2a318bf45be2f19b2709b01dcce033f00fb2b7
subhajitchikky <30743753+subhajitchikky@users.noreply.github.com>
2017-08-25 19:06:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,16 @@
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
   
   var request = new XMLHttpRequest();
-  counter=counter+1;
-  var span= document.getElementById('count');
-  span.innerHTML = counter.toString();
+  request.onreadystatechange = function(){
+   if(request.readystate === XMLHttpRequest.DONE){
+       if(request.status === 200){
+           var counter = request.responseText;
+           var span= document.getElementById('count');
+            span.innerHTML = counter.toString();
+       }
+   }   
+  };
+  request.open('GET', 'http://subhajitchikky.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
