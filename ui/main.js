fd7296a1acbe27da1e04418f95ab384c86dd99a8
subhajitchikky <30743753+subhajitchikky@users.noreply.github.com>
2017-08-25 21:54:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ submit.onclick = function(){
     var names= ['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<names.length;i++){
-        list+= '<li>'+ name[i] +'</li>';
+        list+= '<li>'+name[i]+'</li>';
     }
     var ul= document.getElementById('namelist');
     ul.innerHTML = list;
